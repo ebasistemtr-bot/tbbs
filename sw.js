922b1810290d1bef76d0d143a7eee89219e16b4f
Mustafa <ebasistemtr@gmail.com>
2026-05-02 23:51:24 +0300

    PWA (Mobil Uygulama) altyapısı, yeni logolar ve Gelecek Yarışlar eklendi

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "TBBSY-v11";
+const CACHE_NAME = "TBBSY-v12";
 
 self.addEventListener("install", (e) => {
     console.log("[Service Worker] Motor Kuruluyor... Versiyon:", CACHE_NAME);
